c3df4b0478ca4b70eaee3d762015e6779db0404e
suhwan.yun <suhwan.yun@linecorp.com>
2022-03-11 23:04:26 +0900

    roadmap pc, mobile

--- src/views/Roadmap/Roadmap.js
+++ src/views/Roadmap/Roadmap.js
@@ -1,11 +1,20 @@
 import Main from 'layouts/Main';
 import Container from "../../components/Container";
 import React from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const Roadmap = (props) => {
+  const width870 = useMediaQuery('(max-width:870px)');
+
   return (
     <Main>
-      <img style={{width: "100%", marginBottom: "-10px"}} src={"/roadmap.jpeg"}/>
+      {
+        width870 ?
+          <img style={{width: "100%", marginBottom: "-10px"}} src={"/roadmap_mo.jpeg"}/>
+          :
+          <img style={{width: "100%", marginBottom: "-10px"}} src={"/roadmap_pc.jpeg"}/>
+      }
+
     </Main>
   )
 }
