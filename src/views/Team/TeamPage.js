2f2f350c633cd06afd2b59843cefb976f60e459c
suhwan.yun <suhwan.yun@linecorp.com>
2022-03-14 23:59:02 +0900

    mo cursor

--- src/views/Team/TeamPage.js
+++ src/views/Team/TeamPage.js
@@ -13,10 +13,11 @@ const TeamPage = () => {
     {"width":"6.023255813953488%","height":"3.0937499999999996%","left":"22.209302325581394%","top":"49.84375%", "href": "https://twitter.com/hollycat_box", style: {cursor: "pointer"}},
     {"width":"6.023255813953488%","height":"3.0937499999999996%","left":"69.76744186046511%","top":"50.31250000000001%", "href": "https://twitter.com/cryptodevgang", style: {cursor: "pointer"}}]
 
-  const moMap = [{"width":"8.930232558139535%","height":"3.6666666666666665%","left":"40.8139534883721%","top":"19.280575539568346%", "href": "https://twitter.com/una_ccw"},
-    {"width":"8.930232558139535%","height":"3.6666666666666665%","left":"46.51162790697675%","top":"30.851318944844124%", "href": "https://twitter.com/sp100xx"},
-    {"width":"8.930232558139535%","height":"3.6666666666666665%","left":"13.372093023255822%","top":"49.49640287769784%", "href": "https://twitter.com/hollycat_box"},
-    {"width":"8.930232558139535%","height":"3.6666666666666665%","left":"77.90697674418607%","top":"51.89448441247002%", "href": "https://twitter.com/cryptodevgang"}]
+  const moMap = [{"width":"8.930232558139535%","height":"3.6666666666666665%","left":"40.8139534883721%","top":"19.280575539568346%", "href": "https://twitter.com/una_ccw", style: {cursor: "pointer"}},
+    {"width":"8.930232558139535%","height":"3.6666666666666665%","left":"46.51162790697675%","top":"30.851318944844124%", "href": "https://twitter.com/sp100xx", style: {cursor: "pointer"}},
+    {"width":"8.930232558139535%","height":"3.6666666666666665%","left":"13.372093023255822%","top":"49.49640287769784%", "href": "https://twitter.com/hollycat_box", style: {cursor: "pointer"}},
+    {"width":"8.930232558139535%","height":"3.6666666666666665%","left":"77.90697674418607%","top":"51.89448441247002%", "href": "https://twitter.com/cryptodevgang", style: {cursor: "pointer"}}]
+
   const onMapClick = (area, index) => {
     window.open(area.href, "_blank")
   }
